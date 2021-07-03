dd25fbbfc46e690bd6c1be750ec5c5d377dbb600
Siam11651 <siam11651@outlook.com>
2021-07-03 19:56:48 +0600

    Font size text field don't take empty field

--- src/Pumpa/SettingsController.java
+++ src/Pumpa/SettingsController.java
@@ -1,25 +1,15 @@
 package Pumpa;
 
-import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
 import javafx.stage.Stage;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.net.URL;
-import java.nio.ByteBuffer;
 import java.util.ResourceBundle;
 
 public class SettingsController implements Initializable
@@ -27,7 +17,7 @@ public class SettingsController implements Initializable
     @FXML
     ChoiceBox<String> fx_choice_box_font_name;
     @FXML
-    TextField fx_choice_box_font_size;
+    TextField fx_text_field_font_size;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
@@ -38,9 +28,9 @@ public class SettingsController implements Initializable
         }
 
         fx_choice_box_font_name.setValue(Main.fontName);
-        fx_choice_box_font_size.setText(String.valueOf(Main.fontSize));
+        fx_text_field_font_size.setText(String.valueOf(Main.fontSize));
 
-        fx_choice_box_font_size.textProperty().addListener((ObservableValue<? extends String> observableValue,
+        fx_text_field_font_size.textProperty().addListener((ObservableValue<? extends String> observableValue,
                                                             String oldValue, String newValue)->
         {
             for(int i = 0; i < newValue.length(); i++)
@@ -61,7 +51,7 @@ public class SettingsController implements Initializable
 
                     newValue = suff + preff;
 
-                    fx_choice_box_font_size.setText(oldValue);
+                    fx_text_field_font_size.setText(oldValue);
 
                     break;
                 }
@@ -80,19 +70,34 @@ public class SettingsController implements Initializable
 
             if(size > 48)
             {
-                fx_choice_box_font_size.setText("48");
+                fx_text_field_font_size.setText("48");
             }
             else if(size < 1)
             {
-                fx_choice_box_font_size.setText("1");
+                fx_text_field_font_size.setText("1");
             }
         });
     }
 
-    void ApplySettings()
+    boolean ApplySettings()
     {
-        Main.fontName = fx_choice_box_font_name.getValue();
-        Main.fontSize = Integer.parseInt(fx_choice_box_font_size.getText());
+        if(fx_text_field_font_size.getText().isEmpty())
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "", ButtonType.OK);
+
+            alert.setTitle("Invalid Input");
+            alert.setHeaderText("Do not keep the field empty");
+            alert.show();
+
+            return false;
+        }
+        else
+        {
+            Main.fontName = fx_choice_box_font_name.getValue();
+            Main.fontSize = Integer.parseInt(fx_text_field_font_size.getText());
+
+            return true;
+        }
     }
 
     public void Action_settings_apply(ActionEvent actionEvent)
@@ -107,7 +112,20 @@ public class SettingsController implements Initializable
 
     public void Action_settings_ok(ActionEvent actionEvent)
     {
-        ApplySettings();
-        ((Stage)((Node)actionEvent.getSource()).getScene().getWindow()).close();
+        if(ApplySettings())
+        {
+            ((Stage)((Node)actionEvent.getSource()).getScene().getWindow()).close();
+        }
+    }
+
+    public void Action_font_size(ActionEvent actionEvent)
+    {
+        if(fx_text_field_font_size.getText().isEmpty())
+        {
+            String text = String.valueOf(Main.fontSize);
+
+            fx_text_field_font_size.setText(text);
+            fx_text_field_font_size.positionCaret(text.length());
+        }
     }
 }
